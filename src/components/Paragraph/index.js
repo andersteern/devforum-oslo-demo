2b484e5d5a9d56942544f61b80faf129eea8a4b0
Andern Teern <andersk2@gmail.com>
2018-09-24 23:06:32 +0200

    initial commit
    create react app
    
    ejected
    
    use custom jest runner
    
    star wars

---
+++ src/components/Paragraph/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Text } from 'rebass';
+
+const Paragraph = (props) => <Text mb={2} {...props} />;
+
+export { Paragraph };
---
+++ src/components/Spacing/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Box } from 'rebass';
+
+const Spacing = (props) => <Box px={3} py={4} {...props} />;
+
+export { Spacing };
